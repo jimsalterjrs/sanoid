e26671c97dd48ca45f851f5b737de6c2067747d3
Aaron Whitehouse <code@whitehouse.kiwi.nz>
2022-03-17 10:28:10 +0000

    Add monitoring test framework

---
+++ tests/0_monitoring_tests/test_monitoring.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# this software is licensed for use under the Free Software Foundation's GPL v3.0 license, as retrieved
+# from http://www.gnu.org/licenses/gpl-3.0.html on 2014-11-17.  A copy should also be available in this
+# project's Git repository at https://github.com/jimsalterjrs/sanoid/blob/master/LICENSE.
+
+
+import unittest
+
+class TestNothing(unittest.TestCase):
+    def test_nothing(self):
+        """Test"""
+
+        # Test sanoid_snapshots_exist
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
